dfe2d0ecd1ffbf9a43a1208c503aee426a671eb8
isro24 <alamat_email_anda@example.com>
2024-05-06 16:01:35 +0700

    perbaiki error

--- friendclass2.cpp
+++ friendclass2.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa{
-    private:
+class mahasiswa {
+private:
     string nama;
 
-    public:
-    friend void setnama(mahasiswa &a, string);
+public:
+    friend void setNama(mahasiswa &a, string);
 };
-void setNama(mahasiswa &a, string pNama){
+
+void setNama(mahasiswa &a, string pNama) {
     a.nama = pNama;
 }
 
-int main(){
+int main() {
     mahasiswa joko;
     setNama(joko, "Kairi Kumar");
     return 0;
